17690ce92552374105e1121961def7c129c98d66
Andriamahery <andriamahery@gmail.com>
2026-03-15 09:52:36 +0100

    fix render port

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
 const express = require('express');
-const { PORT } = require('../config/config');
 
 const app = express();
 
-// Sert les fichiers HTML/CSS/JS
+// Render fournit le port automatiquement
+const PORT = process.env.PORT || 10000;
+
+// Sert les fichiers statiques
 app.use(express.static('public'));
 
-app.listen(PORT, () => {
-  console.log(`🚀 Serveur Express lancé sur http://localhost:${PORT}`);
+// route test pour Render
+app.get('/', (req, res) => {
+  res.send('Wingo Bot is running');
 });
 
-app.get('/', (req, res) => {
-  res.send('Bot is running');
+// démarrage serveur
+app.listen(PORT, () => {
+  console.log(`🚀 Serveur Express lancé sur le port ${PORT}`);
 });
